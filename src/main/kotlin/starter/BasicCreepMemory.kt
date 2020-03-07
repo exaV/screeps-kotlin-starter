c39a303adf8e9ad9e61f969427803451c8a67fd3
codedraughtsman <codedraughtsman@users.noreply.github.com>
2020-03-07 14:48:31 +1300

    Expanded BasicCreepMemory and moved it to BasicMemory
    Now BasicMemory sets up persistent memory for all classes.

--- src/main/kotlin/starter/BasicCreepMemory.kt
+++
@@ -1,9 +0,0 @@
-package starter
-
-import screeps.api.CreepMemory
-import screeps.utils.memory.memory
-
-
-var CreepMemory.building: Boolean by memory { false }
-var CreepMemory.pause: Int by memory { 0 }
-var CreepMemory.role by memory(Role.UNASSIGNED)
