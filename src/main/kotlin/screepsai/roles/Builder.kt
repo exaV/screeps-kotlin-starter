6da1b3fa30f935739fad6249ef44ae5b77a0cdc2
Jordan Cottle <jordancottle622@gmail.com>
2022-03-09 23:38:38 -0500

    Get energy from storage

--- src/main/kotlin/screepsai/roles/Builder.kt
+++ src/main/kotlin/screepsai/roles/Builder.kt
@@ -12,19 +12,31 @@ class Builder(creep: Creep) : Role(creep) {
         when (state) {
             CreepState.GET_ENERGY -> {
                 getEnergy()
+                if (creep.store.getFreeCapacity() == 0) {
+                    info("Energy full", say = true)
+                    state = CreepState.DO_WORK
+                }
             }
-            CreepState.DO_WORK -> {
+            CreepState.DO_WORK    -> {
                 buildBuildings()
             }
         }
     }
 
     private fun getEnergy() {
-        pickupEnergy()
+        val storage = creep.room.storage
 
-        if (creep.store.getFreeCapacity() == 0) {
-            info("Energy full", say = true)
-            state = CreepState.DO_WORK
+        if (storage == null || (storage.store.getUsedCapacity(RESOURCE_ENERGY) ?: 0) <= 0) {
+            pickupEnergy()
+            return
+        }
+
+        val code = creep.withdraw(storage, RESOURCE_ENERGY)
+        if (code == ERR_NOT_IN_RANGE) {
+            creep.moveTo(storage)
+        }
+        else if (code != OK) {
+            error("Couldn't withdraw from storage due to error: $code")
         }
     }
 
@@ -42,11 +54,13 @@ class Builder(creep: Creep) : Role(creep) {
 
         if (status == ERR_NOT_IN_RANGE) {
             creep.moveTo(constructionSite)
-        } else if (status == ERR_NOT_ENOUGH_ENERGY) {
+        }
+        else if (status == ERR_NOT_ENOUGH_ENERGY) {
             info("Out of energy", say = true)
             state = CreepState.GET_ENERGY
             return
-        } else if (status != OK) {
+        }
+        else if (status != OK) {
             error("Build failed with code $status", say = true)
         }
 
@@ -69,11 +83,13 @@ class Builder(creep: Creep) : Role(creep) {
 
         if (status == ERR_NOT_IN_RANGE) {
             creep.moveTo(building)
-        } else if (status == ERR_NOT_ENOUGH_ENERGY) {
+        }
+        else if (status == ERR_NOT_ENOUGH_ENERGY) {
             info("Out of energy", say = true)
             state = CreepState.GET_ENERGY
             return
-        } else if (status != OK) {
+        }
+        else if (status != OK) {
             error("Repair failed with code $status", say = true)
         }
 
